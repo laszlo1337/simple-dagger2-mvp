6887a2f22f25ab05df07af992419c2a7c9b4d0d2
Leszek Janiszewski <leszek.janiszewski@zaven.pl>
2017-04-04 18:14:47 +0200

    added GithubInterface, GithubModule

---
+++ app/src/main/java/com/example/leszek/simpledagger2mvp/domain/GithubInterface.java
@@ -0,0 +1,10 @@
+package com.example.leszek.simpledagger2mvp.domain;
+
+/**
+ * Created by Leszek on 04.04.2017.
+ */
+
+public interface GithubInterface {
+
+
+}
---
+++ app/src/main/java/com/example/leszek/simpledagger2mvp/domain/GithubModule.java
@@ -0,0 +1,10 @@
+package com.example.leszek.simpledagger2mvp.domain;
+
+/**
+ * Created by Leszek on 04.04.2017.
+ */
+
+public class GithubModule {
+
+
+}
