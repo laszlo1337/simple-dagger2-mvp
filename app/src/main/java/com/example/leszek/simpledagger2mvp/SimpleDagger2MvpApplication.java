aa440cd49458084ac0b85c7f79e30906a563059c
Leszek Janiszewski <leszek.janiszewski@zaven.pl>
2017-04-04 18:27:49 +0200

    Add Application object

---
+++ app/src/main/java/com/example/leszek/simpledagger2mvp/SimpleDagger2MvpApplication.java
@@ -0,0 +1,30 @@
+package com.example.leszek.simpledagger2mvp;
+
+import android.app.Application;
+import android.support.annotation.NonNull;
+
+import com.example.leszek.simpledagger2mvp.di.component.ApplicationComponent;
+import com.example.leszek.simpledagger2mvp.di.component.DaggerApplicationComponent;
+import com.example.leszek.simpledagger2mvp.di.module.ApplicationModule;
+import com.example.leszek.simpledagger2mvp.di.module.CommonModule;
+
+public class SimpleDagger2MvpApplication extends Application {
+
+    private ApplicationComponent applicationComponent;
+
+    @Override
+    public void onCreate(){
+        super.onCreate();
+
+        applicationComponent = DaggerApplicationComponent.builder()
+                .applicationModule(new ApplicationModule(this))
+                .commonModule(new CommonModule("https://api.github.com/"))
+                .build();
+    }
+
+
+    @NonNull
+    public ApplicationComponent getApplicationComponent() {
+        return applicationComponent;
+    }
+}
