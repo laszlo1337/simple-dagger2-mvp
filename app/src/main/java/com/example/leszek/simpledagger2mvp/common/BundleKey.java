aa18c52a9c7fe8b6ae6847d8822b356e03559ca5
Leszek Janiszewski <leszek.janiszewski@zaven.pl>
2017-04-07 11:43:14 +0200

    Add BundleKey class

---
+++ app/src/main/java/com/example/leszek/simpledagger2mvp/common/BundleKey.java
@@ -0,0 +1,14 @@
+package com.example.leszek.simpledagger2mvp.common;
+
+/**
+ * @author Leszek Janiszewski
+ */
+
+public class BundleKey {
+    private BundleKey(){}
+
+    public static final String KEY_USER_LOGIN = "USER_LOGIN";
+
+
+
+}
