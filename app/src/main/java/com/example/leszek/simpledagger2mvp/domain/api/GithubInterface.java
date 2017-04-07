20a409074ce79709ae78b484530b9aca97e124c5
Leszek Janiszewski <leszek.janiszewski@zaven.pl>
2017-04-07 11:44:05 +0200

    Add interface method for user details call

--- app/src/main/java/com/example/leszek/simpledagger2mvp/domain/api/GithubInterface.java
+++ app/src/main/java/com/example/leszek/simpledagger2mvp/domain/api/GithubInterface.java
@@ -1,11 +1,14 @@
 package com.example.leszek.simpledagger2mvp.domain.api;
 
 import com.example.leszek.simpledagger2mvp.domain.model.User;
+import com.example.leszek.simpledagger2mvp.presentation.userdetails.UserDetails;
 
 import java.util.List;
 
 import io.reactivex.Observable;
+import io.reactivex.Single;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
 import retrofit2.http.Query;
 
 
@@ -14,6 +17,6 @@ public interface GithubInterface {
     @GET("users")
     Observable<List<User>> getUsers(@Query("per_page") int perPage, @Query("since") int lastUserId);
 
-//    @GET("users/{user}")
-//    Observable
+    @GET("users/{user}")
+    Single<UserDetails> getUserDetails(@Path("user") String user);
 }
