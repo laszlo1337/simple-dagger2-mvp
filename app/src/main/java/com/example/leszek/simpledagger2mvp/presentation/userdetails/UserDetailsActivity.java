815123a798b12102701f0b5d66f9fe1aad46ff22
Leszek Janiszewski <leszek.janiszewski@zaven.pl>
2017-04-07 11:45:06 +0200

    Add UserDetailsActivity

--- app/src/main/java/com/example/leszek/simpledagger2mvp/presentation/userdetails/UserDetailsActivity.java
+++ app/src/main/java/com/example/leszek/simpledagger2mvp/presentation/userdetails/UserDetailsActivity.java
@@ -1,12 +1,39 @@
 package com.example.leszek.simpledagger2mvp.presentation.userdetails;
 
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
-/**
- * @author Leszek Janiszewski
- */
+import com.example.leszek.simpledagger2mvp.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class UserDetailsActivity extends AppCompatActivity {
 
+    @BindView(R.id.userdetails_toolbar)
+    Toolbar toolbar;
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_details);
+        ButterKnife.bind(this);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.userdetails_fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
 }
